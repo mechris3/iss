54e1f367df129ef8dc9ad27accc6616101ea700b
cawmichae <chris.michael@sap.com>
2022-11-15 16:48:17 +0000

    ISS Tracker Working

--- main.js
+++ main.js
@@ -1,3 +1,66 @@
 ;(function () {
-  console.log('loaded')
+  console.log('loaded here')
+  class D3Map {
+    constructor(topology) {
+      this.svg = d3.select('body').append('svg').attr('id', 'map')
+      const { height, width } = document
+        .getElementById('map')
+        .getBoundingClientRect()
+      const geojson = topojson.feature(topology, topology.objects['world.geo'])
+      this.countries = geojson.features
+      this.projection = d3.geoMercator()
+      this.projection.fitExtent(
+        [
+          [0, 0],
+          [width, height],
+        ],
+        geojson,
+      )
+    }
+
+    drawCountries() {
+      const path = d3.geoPath().projection(this.projection)
+
+      const countryGroup = this.svg
+        .append('g')
+        .selectAll('path')
+        .data(this.countries)
+        .enter()
+        .append('path')
+        .attr('d', path)
+        .attr('fill', 'lightgray')
+        .attr('stroke', 'white')
+
+      return countryGroup
+    }
+
+    getIss() {
+      d3.json('http://api.open-notify.org/iss-now.json').then((issPosition) => {
+        // console.log('ISS position', issPosition)
+        this.drawISS(issPosition.iss_position)
+      })
+    }
+
+    drawISS(position) {
+      console.log('draw ISS', position)
+      const circleRadius = 1
+      const ISSGroup = this.svg.append('g')
+      ISSGroup.selectAll('circle')
+        .data([position])
+        .enter()
+        .append('circle')
+        .attr('cx', (iss) => {
+          console.log(iss)
+          return this.projection([iss.longitude, iss.latitude])[0]
+        })
+        .attr('cy', (iss) => this.projection([iss.longitude, iss.latitude])[1])
+        .attr('r', circleRadius + 'px')
+        .attr('fill', 'tomato')
+    }
+  }
+  d3.json('world.topo.json').then((topology) => {
+    const map = new D3Map(topology)
+    map.drawCountries()
+    setInterval(() => map.getIss(), 500)
+  })
 })()
