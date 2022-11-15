604080d181fb1e075c1d3ef018bd259f09c32119
cawmichae <chris.michael@sap.com>
2022-11-15 17:07:57 +0000

    Change the API used to https

--- main.js
+++ main.js
@@ -35,10 +35,12 @@
     }
 
     getIss() {
-      d3.json('http://api.open-notify.org/iss-now.json').then((issPosition) => {
-        // console.log('ISS position', issPosition)
-        this.drawISS(issPosition.iss_position)
-      })
+      d3.json('https://api.wheretheiss.at/v1/satellites/25544').then(
+        (issPosition) => {
+          console.log('ISS position', issPosition)
+          this.drawISS(issPosition)
+        },
+      )
     }
 
     drawISS(position) {
@@ -49,10 +51,7 @@
         .data([position])
         .enter()
         .append('circle')
-        .attr('cx', (iss) => {
-          console.log(iss)
-          return this.projection([iss.longitude, iss.latitude])[0]
-        })
+        .attr('cx', (iss) => this.projection([iss.longitude, iss.latitude])[0])
         .attr('cy', (iss) => this.projection([iss.longitude, iss.latitude])[1])
         .attr('r', circleRadius + 'px')
         .attr('fill', 'tomato')
@@ -60,8 +59,8 @@
   }
   d3.json('world.topo.json').then((topology) => {
     const map = new D3Map(topology)
-    map.drawCountries();
-	map.getIss();
-    setInterval(() => map.getIss(), 50000)
+    map.drawCountries()
+    map.getIss()
+    setInterval(() => map.getIss(), 500)
   })
 })()
