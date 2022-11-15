905dafa15d9d5e0d922c75d022475918c6b4388e
cawmichae <chris.michael@sap.com>
2022-11-15 17:42:22 +0000

    Add ISS icon, check every 10 seconds

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 ;(function () {
-  console.log('loaded here')
   class D3Map {
     constructor(topology) {
       this.svg = d3.select('body').append('svg').attr('id', 'map')
@@ -37,30 +36,28 @@
     getIss() {
       d3.json('https://api.wheretheiss.at/v1/satellites/25544').then(
         (issPosition) => {
-          console.log('ISS position', issPosition)
           this.drawISS(issPosition)
         },
       )
     }
 
     drawISS(position) {
-      console.log('draw ISS', position)
-      const circleRadius = 3
       const ISSGroup = this.svg.append('g')
-      ISSGroup.selectAll('circle')
+      ISSGroup.selectAll('image')
         .data([position])
         .enter()
-        .append('circle')
-        .attr('cx', (iss) => this.projection([iss.longitude, iss.latitude])[0])
-        .attr('cy', (iss) => this.projection([iss.longitude, iss.latitude])[1])
-        .attr('r', circleRadius + 'px')
-        .attr('fill', 'tomato')
+        .append('image')
+        .attr('href', 'iss.svg')
+        .attr('width', '15px')
+        .attr('height', '15px')
+        .attr('x', (iss) => this.projection([iss.longitude, iss.latitude])[0])
+        .attr('y', (iss) => this.projection([iss.longitude, iss.latitude])[1])
     }
   }
   d3.json('world.topo.json').then((topology) => {
     const map = new D3Map(topology)
     map.drawCountries()
     map.getIss()
-    setInterval(() => map.getIss(), 5000)
+    setInterval(() => map.getIss(), 10000)
   })
 })()
